7110a8ec1e76abe33554c8e10f6165a0ddc00eda
StijnDeVosSISW <stijn.de_vos@siemens.com>
2020-08-17 15:40:42 +0200

    Add project files.

---
+++ NX_TC_Connector/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NX_TC_Connector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
